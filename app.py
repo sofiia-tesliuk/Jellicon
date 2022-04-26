00621cac0944e90b211ee9853b7873ab03ffb48f
sofiia-tesliuk <sofiia.tesliuk@gmail.com>
2022-04-27 00:07:14 +0300

    Sending icons directly without saving it to the filesystem, added creation of .icns;

--- app.py
+++ app.py
@@ -1,14 +1,9 @@
 #  Created by Sofiia Tesliuk at 2019-09-29.
-import os
+from flask import Flask, render_template, request, send_file
 
-from werkzeug.exceptions import NotFound
-from flask import Flask, render_template, redirect, request, url_for, send_from_directory, after_this_request
-
-from image import UnImage
+from image import ImageProcessor
 
 app = Flask(__name__)
-
-STORAGE_DIRECTORY = "icons"
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -16,9 +11,21 @@ def upload_image():
     if request.method == "POST":
         if request.files:
             image = request.files["image"]
-            if UnImage.valid_format(image.filename):
-                icon_filename = UnImage.create_ico(STORAGE_DIRECTORY, image)
-                return render_template('download.html', icon_filename=icon_filename)
+            if ImageProcessor.valid_format(image.filename):
+                if 'ico' in request.form:
+                    result_icon, icon_filename = ImageProcessor.create_ico(image)
+                    return send_file(result_icon,
+                                     as_attachment=True,
+                                     attachment_filename=icon_filename)
+                elif 'icns' in request.form:
+                    result_icon, icon_filename = ImageProcessor.create_icns(image)
+                    return send_file(result_icon,
+                                     as_attachment=True,
+                                     attachment_filename=icon_filename)
+                else:
+                    error = 'Icon format is not selected.'
+                    return render_template('upload.html', error=error)
+
             else:
                 error = 'Invalid image format.'
                 if image.filename == '':
@@ -27,30 +34,14 @@ def upload_image():
     return render_template('upload.html', error=False)
 
 
-@app.route('/download/<icon_filename>')
-def download_icon(icon_filename):
-    try:
-        @after_this_request
-        def cleanup(response):
-            try:
-                os.remove('{}/{}'.format(STORAGE_DIRECTORY, icon_filename))
-            except FileNotFoundError:
-                return response
-            return response
-
-        return send_from_directory(STORAGE_DIRECTORY, icon_filename, as_attachment=True)
-    except (KeyError, NotFound):
-        return redirect(url_for('upload_image'))
-
-
 @app.errorhandler(400)
 def icon_not_found(error):
-    return render_template('error.html', error='400: Icon not found.'), 400
+    return render_template('error.html', error=error), 400
 
 
 @app.errorhandler(404)
 def page_not_found(error):
-    return render_template('error.html', error='404: Page or icon not found.'), 404
+    return render_template('error.html', error=error), 404
 
 
 if __name__ == '__main__':
--- image.py
+++ image.py
@@ -1,8 +1,9 @@
 #  Created by Sofiia Tesliuk at 2019-09-29.
 from PIL import Image
+from io import BytesIO
 
 
-class UnImage:
+class ImageProcessor:
     icon_sizes = [(16, 16),
                   (32, 32),
                   (64, 64),
@@ -19,23 +20,30 @@ class UnImage:
             return False
 
     @staticmethod
-    def create_icns(path, image):
-        return UnImage._create_icon(path, image, 'icns')
-
-    @staticmethod
-    def create_ico(path, image):
-        return UnImage._create_icon(path, image, 'ico')
-
-    @staticmethod
-    def _create_icon(path, image, extension):
-        img = Image.open(image)
-        img = UnImage._add_transparent_background(img)
+    def _icon_filename(image, extension):
         try:
             icon_filename = '{}.{}'.format(image.filename.split('.')[0], extension)
         except IndexError:
             icon_filename = 'icon.{}'.format(extension)
-        img.save('{}/{}'.format(path, icon_filename), sizes=UnImage.icon_sizes)
         return icon_filename
+
+    @staticmethod
+    def create_ico(image):
+        return ImageProcessor._create_icon(image, 'ICO'), ImageProcessor._icon_filename(image, 'ico')
+
+    @staticmethod
+    def create_icns(image):
+        return ImageProcessor._create_icon(image, 'ICNS'), ImageProcessor._icon_filename(image, 'icns')
+
+    @staticmethod
+    def _create_icon(image, format):
+        img = Image.open(image)
+        img = ImageProcessor._add_transparent_background(img)
+
+        imagefile = BytesIO()
+        img.save(imagefile, format=format, sizes=ImageProcessor.icon_sizes)
+        imagefile.seek(0)
+        return imagefile
 
     @staticmethod
     def _add_transparent_background(image):
